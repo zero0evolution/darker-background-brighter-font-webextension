6484eeb60283a4887a0751c735c73b591a1c6829
zero0evolution <11463831+zero0evolution@users.noreply.github.com>
2018-01-05 23:32:59 +0800

    delete ignore file

--- OptionPage/test/button panel_files/urlToDomain.js
+++
@@ -1,8 +0,0 @@
-var urlToDomain = function(url){
-	var domain = url
-	// 解析 domain
-	var domainMatchObj = url.match(/^(.*?[^\/])\/(?=[^\/]|$)/)
-	if(domainMatchObj){domain = domainMatchObj[1]}
-
-	return(domain)
-}
